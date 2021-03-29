74ffe30e0c2b3a0e8fe87d4c57c1822bc7bfe11b
Sukhrob <webmaster83@mail.ru>
2021-03-29 11:59:23 +0500

    тестим

--- views/site/about.php
+++ views/site/about.php
@@ -10,6 +10,9 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <h2><?= 'тест' ?></h2>
+
+
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
